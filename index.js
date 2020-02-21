a7048d089f89aff3269e817709c4515e1e6f0c9f
Cbustamante777 <cra.bustamante@alumnos.duoc.cl>
2020-02-20 21:16:45 -0300

    modulo de saludos creado

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+module.exports = {
+    hola: function() {
+        console.log('hola mundo');
+    },
+    holaPersonalizado:function (nombre) {
+        console.log(`hola ${nombre}`);
+    },
+    holaNPM: function () {
+        console.log('hola funcion desde npm')
+        
+    }
+
+}
